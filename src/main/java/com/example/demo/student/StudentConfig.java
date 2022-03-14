f6ef9f55719d1173b23f7f302dff4b1b358fb152
ifratmitul <ifrat.ikhtear@gmail.com>
2022-03-14 22:18:06 +0600

    intregated db setup

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,29 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student a = new Student(
+                    "Ifrat", LocalDate.of(1995, Month.OCTOBER, 25),
+                    "ifrat@jotno.net");
+
+            Student b = new Student(
+                    "kuddus",
+                     LocalDate.of(1995, Month.OCTOBER, 25),
+                    "ik@jotno.net");
+
+            repository.saveAll(List.of(a,b));
+        };
+    }
+
+}
